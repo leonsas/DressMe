9a6ffc3d3fc30066e9a81bea22b2325315e4dd09
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2013-05-06 22:53:40 -0500

    cleaned up css on suggestion.html, filled in the suggestion.js array

--- assets/js/suggestion.js
+++ assets/js/suggestion.js
@@ -5,19 +5,68 @@ function getURLParameter(name) {
 suggestions = [{
 	category : "business",
 	event : "interview",
-	suggestions : ["Sports jacket, button-down shirt, dress pants, dress shoes",
+	suggestions : ["sports jacket, button-down shirt, dress pants, dress shoes",
 					 "gray suit, tie, dress shoes"]
 }, {
 	category : "business",
 	event : "meeting",
 	suggestions : ["button-down shirt, jeans, sneakers", 
 					"button-down shirt, khakis, sneakers"]
-},
+}, {
+	category : "business",
+	event : "casual",
+	suggestions : ["t-shirt, jeans, sneakers", "button-down shirt, jeans, sneakers",
+	 "t-shirt, shorts, sneakers"]
 
+}, {
+	category : "business",
+	event : "happy",
+	suggestions : ["sports jacket, t-shirt, jeans, leather shoes", 
+	"sports jacket, button-down shirt, jeans, leather shoes", 
+	"button-down shirt, jeans, leather shoes"]
 
+}, {
+	category : "goingout",
+	event : "date",
+	suggestions : ["sports jacket, button-down shirt, dress pants, leather shoes"]
 
+}, {
+	category : "goingout",
+	event : "clubbing",
+	suggestions : ["polo shirt, belt, jeans, street sneakers"]
 
-]
+}, {
+	category : "goingout",
+	event : "semiformal",
+	suggestions : ["button-down shirt, khakis, boat shoes"]
+
+}, {
+	category : "goingout",
+	event : "bar",
+	suggestions : [" button-down shirt, jeans, sneakers", "button-down shirt, khakis, sneakers",
+	"t-shirt, khakis, sneakers"]
+
+}, {
+	category : "athletic",
+	event : "gym",
+	suggestions : ["t-shirt, shorts, gym shoes"]
+
+}, {
+	category : "athletic",
+	event : "outdoor",
+	suggestions : ["t-shirt, shorts, gym shoes"]
+
+}, {
+	category : "traveling",
+	event : "beach",
+	suggestions : ["t-shirt, swim trunks, sunglasses, flip-flops"]
+
+}, {
+	category : "traveling",
+	event : "skiing",
+	suggestions : ["hat, ski-jacket, gloves, snow pants, ski boots"]
+
+}]
 
 $(function() {
 	category = getURLParameter('category');
@@ -27,7 +76,7 @@ $(function() {
 		console.log(suggestions[i]);
 		if (suggestions[i].category == category && suggestions[i].event == event) {
 			current_suggestions = suggestions[i].suggestions;
-			$("#suggestion_text").html("You could wear "+ current_suggestions[0]);
+			$("#suggestion_text").html("You could wear a "+ current_suggestions[0] + ".");
 			console.log("You could wear "+ current_suggestions[0]);
 		}
 
