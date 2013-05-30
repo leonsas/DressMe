75fc0ad77aefd0e2bbc744fff3e7796d4598fee7
Leon Sasson <leonsassonha@gmail.com>
2013-05-30 16:03:27 -0500

    user suggestion page

--- assets/js/usersuggestion.js
+++ assets/js/usersuggestion.js
@@ -2,7 +2,7 @@ function getURLParameter(name) {
 	return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
 }
 
-var currentSuggestionIndex = 0;
+
 var geocoder;
 var img_urls = [];
 var currImgIndex = 0;
@@ -46,48 +46,6 @@ $(function() {
     });
 
 
-	//key from forecast.io
-	var baseURL = 'https://api.forecast.io/forecast/fc590758007d8f0eb51877e6883ffda1/';
-
-	function getGeoLocation() {
-		//check if geolocation is enabled (i.e browser supports it, and users enables it.)
-		if (navigator.geolocation) {
-			//use HTML5 geolocation API with our callback to show the position
-			navigator.geolocation.getCurrentPosition(getConditions);
-		} else {
-			alert("geolocation not supported");
-		}
-	};
-
-	function getConditions(position) {
-		latitude = position.coords.latitude;
-		longitude = position.coords.longitude;
-		/*initialize();
-		 codeLatLng(latitude, longitude);
-		 */
-		queryURL = baseURL + latitude + ',' + longitude + '?exclude=minutely,hourly,alerts,flags';
-		//GET call that returns the geolookup info
-		$.ajax({
-			url : queryURL,
-			dataType : "jsonp",
-			success : function(data) {
-				console.log(data);
-				currentTemp = Math.round(data.currently.temperature);
-				$("#current_feels_like").text('Feels like ' + currentTemp + '\u00B0 F');
-				icon_type = data.daily.icon;
-				displayWeatherAccesories(currentTemp, icon_type);
-
-			}
-		});
-
-	};
-
-	$("#nextSuggestion").click(
-		function (){
-			currImgIndex+=1;
-			displaySuggestion(currImgIndex);
-		}
-		)
 	
 	$("#next_img").click(
 		function (){
@@ -103,7 +61,6 @@ $(function() {
 		}
 		)
 	displaySuggestion(0);
-	getGeoLocation();
 	params = getSuggestionParams()
 	category = params.category;
 	event = params.event;
