b218fb2d740cb662f58cb27d851fd2335b1376fa
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2013-05-31 01:09:51 -0500

    cleaned up header and footer on new pages

--- assets/js/usersuggestion.js
+++ assets/js/usersuggestion.js
@@ -6,8 +6,46 @@ function getURLParameter(name) {
 var geocoder;
 var img_urls = [];
 var currImgIndex = 0;
+var geocoder;
+
 $(function() {
 	var file;
+
+  //key from forecast.io
+  var baseURL = 'https://api.forecast.io/forecast/fc590758007d8f0eb51877e6883ffda1/';
+
+  function getGeoLocation() {
+    //check if geolocation is enabled (i.e browser supports it, and users enables it.)
+    if (navigator.geolocation) {
+      //use HTML5 geolocation API with our callback to show the position
+      navigator.geolocation.getCurrentPosition(getConditions);
+    } else {
+      alert("geolocation not supported");
+    }
+  };
+
+  function getConditions(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    /*initialize();
+     codeLatLng(latitude, longitude);
+     */
+    queryURL = baseURL + latitude + ',' + longitude + '?exclude=minutely,hourly,alerts,flags';
+    //GET call that returns the geolookup info
+    $.ajax({
+      url : queryURL,
+      dataType : "jsonp",
+      success : function(data) {
+        console.log(data);
+        currentTemp = Math.round(data.currently.temperature);
+        $("#current_feels_like").text('Feels like ' + currentTemp + '\u00B0 F');
+        icon_type = data.daily.icon;
+        displayWeatherAccesories(currentTemp, icon_type);
+
+      }
+    });
+
+  };
 
     // Set an event listener on the Choose File field.
     $('#fileselect').bind("change", function(e) {
@@ -60,6 +98,7 @@ $(function() {
 			setCrowdsourcedImg(currentSuggestionIndex);
 		}
 		)
+  getGeoLocation();
 	displaySuggestion(0);
 	params = getSuggestionParams()
 	category = params.category;
